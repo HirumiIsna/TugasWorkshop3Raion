7a952aeff6592cad240913f97e6510976cf92565
HirumiIsna <hilmiisnaeni@student.ub.ac.id>
2026-04-23 22:12:30 +0700

    feat: Added Jump(With Variable Gravity) + Refactor Code a bit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,16 +3,36 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private PlayerInput _playerInput;
-    public Rigidbody2D rb;
+    [Header("Player Movement")]
+    private Rigidbody2D _rb;
     public float speed;
+    public float jumpForce;
+    public float jumpMultiplier = .5f;
+    public float normalGravity;
+    public float jumpGravity;
+    public float fallGravity;
 
-    public Vector2 moveInput;
+    //Inputs
+    [SerializeField] private Vector2 _moveInput;
+    private PlayerInput _playerInput;
+    private bool startJump;
+    private bool cancelJump;
+    
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool _isGround;
 
     void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        _rb.gravityScale = normalGravity;
     }
 
     void Update()
@@ -22,25 +42,78 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        float targetSpeed = moveInput.x * speed;
-        rb.linearVelocity = new Vector2(targetSpeed, rb.linearVelocity.y);
+        DynamicGravity();
+        CheckGround();
+        HandleMovement();
+        HandleJump();
     }
 
-    public void Move(InputAction.CallbackContext input)
+    public void InputMove(InputAction.CallbackContext input)
     {
-        Debug.Log("Move input = " + input.ReadValue<Vector2>());
-        moveInput = input.ReadValue<Vector2>();
+        _moveInput = input.ReadValue<Vector2>();
+    }
+
+    public void InputJump(InputAction.CallbackContext input) //besok kurapiin lagi
+    {
+        if (input.started) 
+        {
+            startJump = true;
+            cancelJump = false;
+        }
+        else if(input.canceled) 
+        {
+            cancelJump = true;
+        }
+    }
+
+    private void HandleMovement()
+    {
+        float targetSpeed = _moveInput.x * speed;
+        _rb.linearVelocity = new Vector2(targetSpeed, _rb.linearVelocity.y);
+    }
+
+    private void HandleJump() //besok kurapiin lagi
+    {
+        if (startJump && _isGround) 
+        {
+            startJump = false;
+            cancelJump = false;
+            _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpForce);
+        }
+        if (cancelJump)
+        {
+            cancelJump = false;
+            if(_rb.linearVelocity.y > 0) _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, _rb.linearVelocity.y * jumpMultiplier);
+        }
+    }
+
+    public void DynamicGravity()
+    {
+        if (_rb.linearVelocity.y < -0.1f) _rb.gravityScale = fallGravity;
+        else if (_rb.linearVelocity.y > 0.1f) _rb.gravityScale = jumpGravity;
+        else _rb.gravityScale = normalGravity;
+    }
+
+    public void CheckGround()
+    {
+        _isGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 
     public void FlipSprite()
     {
-        if (moveInput.x > 0)
+        if (_moveInput.x > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
         }
-        else if (moveInput.x < 0)
+        else if (_moveInput.x < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
+    } 
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);        
     }
 }
